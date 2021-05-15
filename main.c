7fb6b0cfb9ec8cbac27d1674e9ddb9728c653a19
f4tih-0z3r <fatihozer122333@gmail.com>
2021-05-15 13:41:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 
+#define RED		"\x1b[31m"
+#define GREEN	"\x1b[32m"
+#define YELLOW	"\x1b[33m"
+#define BLUE	"\x1b[34m"
+#define MAGENTA	"\x1b[35m"
+#define CYAN	"\x1b[36m"
+#define RESET	"\x1b[0m"
+
 int main() {
 	void delay(int milliSeconds) {
 		milliSeconds = milliSeconds * 1000;
@@ -23,8 +31,7 @@ int main() {
 	printf("Whats your name: ");
 	scanf("%s", name);
 
-	printf("Fuck you %s\n", name);
-	delay(1000);
+	printf("Fuck you %s\n", name);delay(1000);
 
 	int age;
 
@@ -47,6 +54,15 @@ int main() {
 	printf("Now x: %d, y: %d\n", x, y);
 	delay(1000);
 	printf("And fuck you %s\n", name);
+
+	printf(RED "I'm RED" RESET "\n");
+	printf(GREEN "I'm GREEN" RESET "\n");
+	printf(YELLOW "I'm YELLOW" RESET "\n");
+	printf(BLUE "I'm BLUE" RESET "\n");
+	printf(MAGENTA "I'm MAGENTA" RESET "\n");
+	printf(CYAN "I'm CYAN" RESET "\n");
+
+	printf(RED "I" GREEN "'" YELLOW "m" BLUE " G" MAGENTA "A" CYAN "Y" RESET "\n");
 	
 	return 0;
 }
